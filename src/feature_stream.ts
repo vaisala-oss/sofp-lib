6a2f5416db0bf40d62d525ca8040bed663b5c811
Sampo Savolainen <sampo.savolainen@spatineo.com>
2018-10-05 11:56:36 +0300

    add documentation and improve filter

--- src/feature_stream.ts
+++ src/feature_stream.ts
@@ -5,6 +5,12 @@ export class FeatureStream extends Readable {
     constructor() {
         super({ objectMode: true, read: () => {} });
     }
+
+    /**
+     * Add feature to stream.
+     *
+     * @returns false if the filter does not accept the feature
+     **/
     push(feature : any) : boolean {
         if (feature !== null && feature !== undefined) {
             for (var i = 0; i < this.remainingFilter.length; i++) {
@@ -15,5 +21,12 @@ export class FeatureStream extends Readable {
         }
         return super.push(feature);
     }
+
+    /**
+     * Array of filters that the backend was unable to pass over to the actual data storage. The stream
+     * will only accept features that are accepted by all remaining filters.
+     *
+     * @see push
+     **/
     remainingFilter : Filter[] = [];
 }
--- src/filter.ts
+++ src/filter.ts
@@ -1,6 +1,24 @@
 import {Feature} from './';
 
 export interface Filter {
+    /**
+     * Unique name for this type of filter
+     **/
     filterClass : string;
+
+    /**
+     * This filter as a query string ('foo=bar')
+     **/
+    asQuery : string;
+    
+    /**
+     * Parameters for the filter, these can be used in backends when filters are used in the backend to
+     * for example produce SQL where parameters.
+     **/
+    parameters : object;
+
+    /**
+     * Synchronous function to determine whether a feature is accepted by the filter
+     **/
     accept(feature : Feature) : boolean;
 };
