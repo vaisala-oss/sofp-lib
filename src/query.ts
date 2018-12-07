c3620fe4722663bddffd0ee05f2ef284e463ad59
Sampo Savolainen <sampo.savolainen@spatineo.com>
2018-12-07 14:49:27 +0200

    Update query.ts
    Added note about query filters

--- src/query.ts
+++ src/query.ts
@@ -3,5 +3,9 @@ import {Filter} from './';
 export interface Query {
     limit : Number;
     nextToken : String;
+    
+    /**
+     * Filter as parsed from the query. Backends should never modify the filter array or the contents.
+     */
     filters : Filter[];
 };
