f51cd2c4e16ea8ed8c8b7528e1c237fb1f31a103
Sampo Savolainen <sampo.savolainen@spatineo.com>
2020-11-05 15:39:54 +0200

    Add optional basePath to ProperyReference when referring to collections within another sofp instance

--- src/property_reference.ts
+++ src/property_reference.ts
@@ -2,15 +2,20 @@ import {Collection} from './collection';
 
 /**
  * This class can be used as values for properties in features returned by a backend.
- * SOFP will formulate links 
+ * SOFP will formulate links to other items. Each link should define the collection
+ * (either a Collection or the id as a string) and the id of the item. Optionally, if
+ * the item resides in another instance of SOFP running parallel (same host) on the same
+ * host, a basePath to the other SOFP can be declared.
  **/
 export class PropertyReference {
     type : string = 'Feature';
     collection : Collection | string;
     id : string;
+    basePath : string | undefined;
 
-    constructor(collection : Collection | string, id: string) {
+    constructor(collection : Collection | string, id: string, basePath?: string) {
         this.collection = collection;
         this.id = id;
+        this.basePath = basePath;
     }
 }
